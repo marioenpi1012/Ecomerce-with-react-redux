bcd62edb6df0473c018413fbad52438d68d76a01
Mario <mariopineda179@gmail.com>
2022-11-09 17:25:19 -0600

    added homepage package.json and basename index.js

--- ecomerce/src/App.jsx
+++ ecomerce/src/App.jsx
@@ -42,7 +42,7 @@ function App() {
         <Nav /> 
         <AnimatePresence exitBeforeEnter>
             <Routes location={location} key={location.pathname}>
-                <Route path='/' exact element={<Home />} />
+                <Route path='/' exact element={<Home />}/>
                 <Route path='/shop' element={isLoading ? <Skeleton /> : <Products />}/>
                 <Route path='/viewing' element={<SelectedProduct />}/>
                 <Route path='/cart' element={<Cart />}/>
